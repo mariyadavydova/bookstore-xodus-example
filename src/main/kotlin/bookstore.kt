2603826c2e941f4e9065bc36a31457be2141792f
Mariya Davydova <mariyafomkina@gmail.com>
2019-02-28 14:21:02 +0300

    Updated entities + several entities to play around

--- src/main/kotlin/bookstore.kt
+++ src/main/kotlin/bookstore.kt
@@ -2,7 +2,8 @@ import jetbrains.exodus.database.TransientEntityStore
 import jetbrains.exodus.entitystore.Entity
 import kotlinx.dnq.*
 import kotlinx.dnq.enum.XdEnumEntityType
-import kotlinx.dnq.link.OnDeletePolicy
+import kotlinx.dnq.query.XdMutableQuery
+import kotlinx.dnq.query.asSequence
 import kotlinx.dnq.store.container.StaticStoreContainer
 import kotlinx.dnq.util.initMetaData
 import java.io.File
@@ -13,31 +14,38 @@ class XdAuthor(entity: Entity) : XdEntity(entity) {
   var name by xdRequiredStringProp()
   var country by xdStringProp()
   var yearOfBirth by xdRequiredIntProp()
-  var yearofDeath by xdIntProp()
+  var yearOfDeath by xdNullableIntProp()
+  val books by xdLink0_N(XdBook::authors)
 
-  val books by xdLink0_N(XdBook, onTargetDelete = OnDeletePolicy.CLEAR)
+  override fun toString(): String {
+    val bibliography = books.asSequence().joinToString("\n")
+    return "$name ($yearOfBirth-${yearOfDeath ?: "???"}):\n$bibliography"
+  }
 }
 
 class XdBook(entity: Entity) : XdEntity(entity) {
   companion object : XdNaturalEntityType<XdBook>()
 
   var title by xdRequiredStringProp()
-  var year by xdIntProp()
-  val genre by xdLink1_N(XdGenre)
+  var year by xdNullableIntProp()
+  val genres by xdLink1_N(XdGenre)
+  val authors : XdMutableQuery<XdAuthor> by xdLink1_N(XdAuthor::books)
 
-  val authors by xdLink0_N(XdAuthor, onTargetDelete = OnDeletePolicy.CASCADE)
+  override fun toString(): String {
+    val genres = genres.asSequence().joinToString(", ")
+    return "$title (${year ?: "Unknown"}) - $genres"
+  }
 }
 
 class XdGenre(entity: Entity) : XdEnumEntity(entity) {
   companion object : XdEnumEntityType<XdGenre>() {
-    val FANTASY by enumField { presentation = "Fantasy" }
-    val ROMANCE by enumField { presentation = "Romance" }
-    val ADVENTURES by enumField { presentation = "A" }
-    val OTHER by enumField { presentation = "-" }
+    val FANTASY by enumField {}
+    val ROMANCE by enumField {}
   }
 
-  var presentation by xdRequiredStringProp()
-    private set
+  override fun toString(): String {
+    return this.name.toLowerCase().capitalize()
+  }
 }
 
 fun initXodus(): TransientEntityStore {
@@ -58,26 +66,60 @@ fun initXodus(): TransientEntityStore {
 fun main() {
   val store = initXodus()
 
-  val author = store.transactional {
+  val bronte = store.transactional {
     XdAuthor.new {
       name = "Charlotte Brontë"
       country = "England"
       yearOfBirth = 1816
-      yearofDeath = 1855
+      yearOfDeath = 1855
     }
   }
 
-  val book = store.transactional {
+  store.transactional {
     XdBook.new {
       title = "Jane Eyre"
       year = 1847
-      genre.add(XdGenre.ROMANCE)
-      authors.add(author)
+      genres.add(XdGenre.ROMANCE)
+      authors.add(bronte)
+    }
+  }
+
+  val tolkien = store.transactional {
+    XdAuthor.new {
+      name = "J. R. R. Tolkien"
+      country = "England"
+      yearOfBirth = 1892
+      yearOfDeath = 1973
+    }
+  }
+
+  store.transactional {
+    tolkien.books.add(XdBook.new {
+      title = "The Hobbit"
+      year = 1937
+      genres.add(XdGenre.FANTASY)
+    })
+    tolkien.books.add(XdBook.new {
+      title = "The Lord of the Rings"
+      year = 1955
+      genres.add(XdGenre.FANTASY)
+    })
+  }
+
+  store.transactional {
+    XdAuthor.new {
+      name = "George R. R. Martin"
+      country = "USA"
+      yearOfBirth = 1948
+      books.add(XdBook.new {
+        title = "A Game of Thrones"
+        year = 1996
+        genres.add(XdGenre.FANTASY)
+      })
     }
   }
 
   store.transactional(readonly = true) {
-    println(author)
-    println(book)
+    println(XdAuthor.all().asSequence().joinToString("\n***\n"))
   }
 }
